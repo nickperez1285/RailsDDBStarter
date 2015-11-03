02c4e3bd1284aa6923fbf67adee3b07b60877b99
Cory At AWS <coryforsythe@Corys-MacBook-Pro.local>
2015-11-03 09:23:16 -0800

    additions

--- RubyDDBStarter/app/controllers/article_controller.rb
+++ RubyDDBStarter/app/controllers/article_controller.rb
@@ -3,13 +3,11 @@ class ArticleController < ApplicationController
 
   def get
     @articleId=params.fetch(:articleId).to_i
-    @article = getSingleItem(@articleId)
+    @article = Article.getSingleItem(@articleId)
     puts "Got article: "+@article.inspect
 
+
   end
 
-  def getSingleItem(id)
-    return Article.new(Aws::DynamoDB::Client.new.get_item(Article.byId(id)).item)
-  end
 
 end
--- RubyDDBStarter/app/models/article.rb
+++ RubyDDBStarter/app/models/article.rb
@@ -2,18 +2,29 @@ class Article
 @@table_name='Articles' #table name
 @@table_projection= "Title" #all fields
 
-def self.byId(id)
- puts "Table Name is "+@@table_name
- puts "Projection to return is "+@@table_projection
-  return {:table_name => @@table_name, :key => {:ArticleId => id},  :projection_expression => @@table_projection}
-end
+#ORM-ish functions
+  def self.getSingleItem(id)
+    return self.new(Aws::DynamoDB::Client.new.get_item(
+        {:table_name => @@table_name, :key => {:ArticleId => id},  :projection_expression => @@table_projection}
+    ).item)
+  end
 
+  def self.SaveItem(id)
+    Aws::DynamoDB::Client.new.put_item({ table_name: @@table_name, item: self.attributes });
+  end
 
+#provides coverage for projection values without specific instnace accessor
 def initialize(hash)
     hash.each do |k,v|
       self.instance_variable_set("@#{k}", v)
       self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
       self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)})
     end
+  end
+#maps current instance variables to a hash for projection
+def attributes
+    instance_variables.map do |var|
+      [var[1..-1].to_sym, instance_variable_get(var)]
+    end.to_h
   end
 end
